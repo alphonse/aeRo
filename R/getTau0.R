d2172d88a5cfa6cc1145be5ad2b9bb6adee4c604
alphonse <alphonse.fisch@gmail.com>
2017-03-06 11:43:45 -0500

    Revert "setNames"
    This reverts commit 44635deac3480752734a91c6c5f9df34d71ac5e7.

--- R/getTau0.R
+++ R/getTau0.R
@@ -1,4 +1,4 @@
 getTau0 <- function(x, period = 1800) {
-  colMeans(matrix(x, nrow = period)[275:325, ]) %>%
-    setNames('bext662_Mm')
+  colMeans(matrix(x, nrow = period)[275:325, ])
+  setNames('bext662_Mm')
 }
