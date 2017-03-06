b11b981d62bbb3c461fc0a1ef74378a0bf294345
alphonse <alphonse.fisch@gmail.com>
2017-03-06 11:22:10 -0500

    chooseFilterStart()
    set start point manually

--- R/chooseFilterStart.R
+++ R/chooseFilterStart.R
@@ -1,11 +1,15 @@
-chooseFilterStart <- function(X, xlim = c(0, 3000), col = 2, period = 1800) {
-  p <- plotly::plot_ly( y = ~X[(xlim[1]:xlim[2]), col], type = 'scatter', mode = 'lines' ) %>%
-    plotly::layout(
-      xaxis = list(title = 'Data Point Number'),
-      yaxis = list(title = 'Mic Intensity for 405nm (mV)')
-    )
-  print(p)
-
-  startpt <- as.numeric(readline("Find start of filter on graph and input here: "))
+chooseFilterStart <- function(X, xlim = c(0, 3000), col = 2, period = 1800, graph = TRUE, start = 1) {
+  if (graph == FALSE) {
+    startpt <- start
+  }
+  else {
+    p <- plotly::plot_ly( y = ~X[(xlim[1]:xlim[2]), col], type = 'scatter', mode = 'lines' ) %>%
+      plotly::layout(
+        xaxis = list(title = 'Data Point Number'),
+        yaxis = list(title = 'Mic Intensity for 405nm (mV)')
+      )
+    print(p)
+    startpt <- as.numeric(readline("Find start of filter on graph and input here: "))
+  }
   X[startpt:((nrow(X)-1)-((nrow(X)-startpt)%%period)), ]
 }
