fbe3471c4ae050608ff7a3a4b240ed449dbaec36
alphonse <alphonse.fisch@gmail.com>
2017-03-07 17:53:06 -0500

    deprecated

--- R/hijack.R
+++
@@ -1,9 +0,0 @@
-hijack <- function (FUN, ...) {
-  .FUN <- FUN
-  args <- list(...)
-  lapply(seq_along(args), function(i) {
-      formals(.FUN)[[names(args)[i]]] <<- args[[i]]
-    }
-  )
-  .FUN
-}
--- R/matplot.hijack.R
+++
@@ -1,6 +0,0 @@
-matplot.hijack <- hijack(matplot,
-                type = 'l', lty = c(rep('solid', 4), 'dashed'), lwd = 2,
-                xlab = 'Time',
-                ylab = expression(paste(alpha[PAS-CRD], ' (M', m^-1, ')')),
-                col = c('purple', 'forestgreen', 'red', 'darksalmon', 'red')
-)
