aa91e63fb50ecb306dbb536bca70c813fd203368
alphonse <alphonse.fisch@gmail.com>
2017-03-13 10:26:48 -0400

    na.omit bug

--- R/blockMeans.single.R
+++ R/blockMeans.single.R
@@ -1,5 +1,5 @@
 blockMeans.single <- function(x, n = 600) {
-  means <- colMeans(stats::na.omit(matrix(x, nrow = n)))
+  means <- colMeans(matrix(stats::na.omit(x), nrow = n))
   if (length(x) %% n != 0) {
     means <- means[-length(means)]
   }
