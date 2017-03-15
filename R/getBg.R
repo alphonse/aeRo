b43a10236d60451bc0937be903b227e54032b7a5
alphonse <alphonse.fisch@gmail.com>
2017-03-15 18:09:33 -0400

    dots

--- R/getBg.R
+++ R/getBg.R
@@ -1,5 +1,8 @@
 getBg <- function(X, cols = 2:5, names = c('bg406_mV', 'bg532_mV', 'bg662_mV', 'bg785_mV'), ...) {
+  dots <- list(...)
+  if (dots[['period']]) { period <- dots[['period']] }
+  else period <- 1800
   apply(X[, cols], 2, getBg.waves, ...) %>%
-    fillToLength(...) %>%
+    fillToLength(period = period) %>%
     stats::setNames(names)
 }
